6bae64d99309359ceb37c7628404f23cca6d7033
Adrian Calero <acalerobarrero@gmail.com>
2022-05-30 09:43:08 +0200

    Solving error for src script

--- dist/detail.js
+++
@@ -1,7 +0,0 @@
-import { Footer } from './components/footer.js';
-import { Header } from './components/header.js';
-const detailApp = async () => {
-    new Header('slot.header');
-    new Footer('slot.footer');
-};
-(() => document.addEventListener('DOMContentLoaded', detailApp))();
